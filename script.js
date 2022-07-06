a6b3dd7dd1bff7b1d588ee002cae405f02c7ae77
EliYakov <eliko0807@gmail.com>
2022-07-06 09:11:34 +0300

    JS

---
+++ script.js
@@ -0,0 +1,68 @@
+console.log({
+  PremierLeague_TopSix: {
+    TitleWinners: {
+      name: "Man City",
+      Points: 93,
+    },
+    TheRunnerUp: {
+      name: "Liverpool",
+      Points: 92,
+    },
+    ThirdPlace: {
+      name: "Chelsea",
+      Points: 74,
+    },
+    ForthPlace: {
+      name: "Spurs",
+      Points: 71,
+    },
+    FifthPlace: {
+      name: "Arsenal",
+      Points: 69,
+    },
+    SixthPlace: {
+      name: "Man Utd",
+      Points: 58,
+    },
+  },
+
+  PremierLeague_RelegationZone: {
+    "18thPlace": {
+      name: "Burnley",
+      Points: 35,
+    },
+    "19thPlace": {
+      name: "Watford",
+      Points: 23,
+    },
+    "20thPlace": {
+      name: "Nowrich City",
+      Points: 22,
+    },
+  },
+
+  ChampionsLeague_Qualifications: {
+    "1stPlace": "Man City",
+    "2ndPlace": "Liverpool",
+    "3rdPlace": "Chelsea",
+    "4thPlace": "Spurs",
+  },
+
+  EuropeLeague_Qualifications: {
+    "5thPlace": "Arsenal",
+    "6thPlace": "Man Utd",
+  },
+
+  ConferenceLeague_Qualifications: {
+    "7thPlace": "WestHam-Utd",
+  },
+
+  QualifiedTO_PremierLeague_From_Championship: {
+    FirstPlace: "Fulham",
+    SecondPlace: "Bournemouth",
+    ThirdPlace: {
+      name: "Huddersfield town",
+      playOffGamesWinners: true,
+    },
+  },
+});
